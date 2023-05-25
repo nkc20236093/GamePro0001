739b01acfaa945cb90b5585b7b26ef378ae79713
nkc20236093 <nck23.taki.eita@gmail.com>
2023-05-25 11:15:54 +0900

    a

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+           
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+        }
+    }
+}
