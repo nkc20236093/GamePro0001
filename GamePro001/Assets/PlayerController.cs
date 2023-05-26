5e93c95e8e0a5aa040ddf13fd60131bcb687ff4d
nkc20236093 <nck23.taki.eita@gmail.com>
2023-05-26 11:14:06 +0900

    更新

--- GamePro001/Assets/PlayerController.cs
+++ GamePro001/Assets/PlayerController.cs
@@ -1,27 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 using static UnityEditor.PlayerSettings;
 
 public class PlayerController : MonoBehaviour
 {
-    float tateidou = Input.GetAxis("Horizontal");
-    float yokoidou = Input.GetAxis("Vertical");
+    float yokoidou;
+    float tateidou;
+    float tateidousokudo;
+    float yokoidousokudo;
+    Vector3 Pos = Vector3.zero;
+    Rigidbody2D Rigid2d;
+    
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 60;
+        this.Rigid2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float pos.y += tateidou;
-        float pos.x += yokoidou;
+        tateidousokudo = Mathf.Abs(this.Rigid2d.velocity.x);
+        yokoidousokudo = Mathf.Abs(this.Rigid2d.velocity.y);
+        yokoidou = Input.GetAxisRaw("Horizontal");
+        tateidou = Input.GetAxisRaw("Vertical");
+        Pos.y = tateidou;
+        Pos.x = yokoidou;
+        transform.position += Pos;
         if (transform.position.y <= 4.3f && transform.position.y >= -4.36f && transform.position.x < 7.99f && transform.position.x > -8.01f)
         {
-
+            this.Rigid2d.AddForce(Pos * ) ;
         }
     }
 }
