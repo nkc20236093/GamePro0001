b79c04c3d2814380fbe9fd7f3d8d50f9021f956c
nkc20236093 <nck23.taki.eita@gmail.com>
2023-05-29 15:11:16 +0900

    更新

--- GamePro001/Assets/PlayerController.cs
+++ GamePro001/Assets/PlayerController.cs
@@ -4,6 +4,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 using static UnityEditor.PlayerSettings;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class PlayerController : MonoBehaviour
 {
@@ -13,27 +14,44 @@ public class PlayerController : MonoBehaviour
     float yokoidousokudo;
     Vector3 Pos = Vector3.zero;
     Rigidbody2D Rigid2d;
+    Animator animator;
     
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 60;
         this.Rigid2d = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        tateidousokudo = Mathf.Abs(this.Rigid2d.velocity.x);
-        yokoidousokudo = Mathf.Abs(this.Rigid2d.velocity.y);
+        yokoidousokudo = 1;//Mathf.Abs(this.Rigid2d.velocity.x);
+        tateidousokudo = 1; Mathf.Abs(this.Rigid2d.velocity.y);
+        
+
         yokoidou = Input.GetAxisRaw("Horizontal");
         tateidou = Input.GetAxisRaw("Vertical");
-        Pos.y = tateidou;
-        Pos.x = yokoidou;
+        
+        Pos.y = tateidou*tateidousokudo;
+        Pos.x = yokoidou*yokoidousokudo;
+
         transform.position += Pos;
-        if (transform.position.y <= 4.3f && transform.position.y >= -4.36f && transform.position.x < 7.99f && transform.position.x > -8.01f)
-        {
-            //this.Rigid2d.AddForce(Pos * ) ;
-        }
+
+        // ˆÚ“®§ŒÀ
+        Vector3 p = transform.position;
+        if (p.y >= 4.3f) p.y = 4.3f;
+        if (p.y <= -4.36f) p.y = -4.36f;
+        if (p.x >= 7.99f) p.x = 7.99f;
+        if (p.x <= -8.01f) p.x = -8.01f;
+
+        transform.position = p;
+
+        //Debug.Log(Pos);
+        //if (transform.position.y <= 4.3f && transform.position.y >= -4.36f && transform.position.x < 7.99f && transform.position.x > -8.01f)
+        //{
+        //    this.Rigid2d.AddForce(Pos);
+        //}
     }
 }
